5f3842ce7ffd71bf1354a21ab2a7c616e2384d3d
67160206 <67160206@go.buu.ac.th>
2025-10-01 11:14:25 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return "Stack is empty!"
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            return "Stack is empty!"
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+#TestProgram
+if __name__ == "__main__":
+    s = Stack()
+    print("Is empty?", s.is_empty())
+
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.push(4)
+    s.push(5)
+
+    print("Size after push:", s.size())
+    print("Top element:", s.peek())
+
+    print("Pop:", s.pop())
+    print("Pop:", s.pop())
+    print("Pop:", s.pop())
+    print("Pop:", s.pop())
+    print("Pop:", s.pop())
+
+    print("Is empty?", s.is_empty())
+    print("Pop from empty:", s.pop())   
